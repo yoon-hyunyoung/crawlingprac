f206b9c779ce8938c804c25188bb9d06906d2724
yoonhyunyoung <taupo28@gmail.com>
2020-09-18 12:28:34 +0900

    삼성 뉴스 크롤링으로 업데이트

--- adapt.py
+++ adapt.py
@@ -1,13 +1,13 @@
 import pymysql
 
-def Insert(news):
+def Insert(ss):
     conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='mysql!1234', db='navernews_test', charset='utf8')
     cursor = conn.cursor()
     
-    query = """insert into newses (headnews) 
+    query = """insert into samsunglionsnews (title) 
             values (%s)
             """
-    cursor.execute(query, (news,))
+    cursor.execute(query, (ss,))
     conn.commit()
     conn.close()
     print(conn)
--- main.py
+++ main.py
@@ -7,14 +7,13 @@ from adapt import Insert
 driver = webdriver.Chrome()
 driver.implicitly_wait(3)
 
-driver.get('https://sports.news.naver.com/index.nhn')
+driver.get('https://sports.news.naver.com/kbaseball/news/index.nhn?isphoto=N&type=team&team=SS&date=20200918')
 
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
-news = soup.select('div.text_area > strong.title')
+news = soup.select('#_newsList > ul > li > div > a > span')
 for n in news:
     print(n.text.strip())
     Insert(n.text.strip())
 
 
-    
